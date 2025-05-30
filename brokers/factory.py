50bb7583df3c954383ca89ce50285ad9a43413e7
AnkitSG07 <ankitsinghgaria@gmail.com>
2025-05-30 09:57:37 +0530

    Update factory.py

--- brokers/factory.py
+++ brokers/factory.py
@@ -1,1 +1,14 @@
+# brokers/factory.py
+
+from .dhan import DhanBroker
+from .zerodha import ZerodhaBroker
+# import other brokers...
+
+def get_broker_class(broker_name):
+    if broker_name.lower() == "dhan":
+        return DhanBroker
+    if broker_name.lower() == "zerodha":
+        return ZerodhaBroker
+    # add more...
+    raise Exception("Unknown broker")
 
