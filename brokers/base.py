2334c756e5ab6c3fc0980cdf7e898f7b6f7b708d
AnkitSG07 <ankitsinghgaria@gmail.com>
2025-05-30 10:00:40 +0530

    Update base.py

--- brokers/base.py
+++ brokers/base.py
@@ -1,1 +1,23 @@
+# brokers/base.py
+
+class BrokerBase:
+    def __init__(self, **kwargs):
+        pass
+
+    @classmethod
+    def login_url(cls, *args, **kwargs):
+        raise NotImplementedError
+
+    @classmethod
+    def get_access_token(cls, *args, **kwargs):
+        raise NotImplementedError
+
+    def get_positions(self):
+        raise NotImplementedError
+
+    def get_order_list(self):
+        raise NotImplementedError
+
+    def place_order(self, *args, **kwargs):
+        raise NotImplementedError
 
