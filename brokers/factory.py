797a326f5e763f7355c90fc5fc047e599f129781
AnkitSG07 <ankitsinghgaria@gmail.com>
2025-05-30 10:23:23 +0530

    Update factory.py

--- brokers/factory.py
+++ brokers/factory.py
@@ -1,21 +1,45 @@
 # brokers/factory.py
 
-from .dhan import DhanBroker
+from .base import BrokerBase
+
 from .zerodha import ZerodhaBroker
 from .aliceblue import AliceBlueBroker
 from .fyers import FyersBroker
-# add other imports as needed
+from .finvasia import FinvasiaBroker
+from .dhan import DhanBroker
+from .flattrade import FlattradeBroker
+from .acagarwal import ACAgarwalBroker
+from .motilaloswal import MotilalOswalBroker
+from .kotakneo import KotakNeoBroker
+from .tradejini import TradejiniBroker
+from .zebu import ZebuBroker
+from .enrichmoney import EnrichMoneyBroker
+from .broker1 import Broker1
 
-def get_broker_class(broker_name):
-    name = broker_name.lower()
-    if name == "dhan":
-        return DhanBroker
-    elif name == "zerodha":
-        return ZerodhaBroker
-    elif name == "aliceblue":
-        return AliceBlueBroker
-    elif name == "fyers":
-        return FyersBroker
-    # add more as you add brokers
-    else:
-        raise Exception(f"Unknown broker: {broker_name}")
+BROKER_CLASS_MAP = {
+    "zerodha": ZerodhaBroker,
+    "aliceblue": AliceBlueBroker,
+    "fyers": FyersBroker,
+    "finvasia": FinvasiaBroker,
+    "dhan": DhanBroker,
+    "flattrade": FlattradeBroker,
+    "acagarwal": ACAgarwalBroker,
+    "motilaloswal": MotilalOswalBroker,
+    "kotakneo": KotakNeoBroker,
+    "tradejini": TradejiniBroker,
+    "zebu": ZebuBroker,
+    "enrichmoney": EnrichMoneyBroker,
+    "broker1": Broker1,  # Custom/unlisted broker logic
+}
+
+def get_broker_instance(broker_name, client_id, access_token, **kwargs):
+    """
+    Factory to instantiate broker objects dynamically.
+    Example usage:
+        broker = get_broker_instance("zerodha", client_id, access_token)
+    """
+    name = str(broker_name or "").lower()
+    broker_class = BROKER_CLASS_MAP.get(name)
+    if not broker_class:
+        raise ValueError(f"Broker '{broker_name}' is not supported in this system.")
+    return broker_class(client_id, access_token, **kwargs)
