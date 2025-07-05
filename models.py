c5b38f3d933c18d495102ab42a760482ce8be2a5
AnkitSG07 <ankitsinghgaria@gmail.com>
2025-07-05 20:01:52 +0530

    Update models.py

--- models.py
+++ models.py
@@ -24,8 +24,8 @@ class User(db.Model):
     is_admin = db.Column(db.Boolean, default=False)
     
     # Added: Timestamps for auditing
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    #created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    #updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def set_password(self, password: str):
         self.password_hash = generate_password_hash(password)
