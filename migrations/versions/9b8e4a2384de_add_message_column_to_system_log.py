b30b5fe0b01909e6b67b60f56b6f8aeef19f8a49
AnkitSG07 <ankitsinghgaria@gmail.com>
2025-07-09 14:32:22 +0530

    Update 9b8e4a2384de_add_message_column_to_system_log.py

--- migrations/versions/9b8e4a2384de_add_message_column_to_system_log.py
+++ migrations/versions/9b8e4a2384de_add_message_column_to_system_log.py
@@ -1,22 +1,21 @@
-"""add message column to system_log
+"""Add message column to system_log table
 
-Revision ID: 9b8e4a2384de
+Revision ID: fix_system_log_20250709
 Revises: 69a08f36e333
-Create Date: 2025-07-10 12:00:00.000000
+Create Date: 2025-07-09 08:54:00
+
 """
 from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = '9b8e4a2384de'
+revision = 'fix_system_log_20250709'
 down_revision = '69a08f36e333'
 branch_labels = None
 depends_on = None
 
-
 def upgrade():
-    op.execute("ALTER TABLE system_log ADD COLUMN IF NOT EXISTS message TEXT")
-
+    op.add_column('system_log', sa.Column('message', sa.Text()))
 
 def downgrade():
-    op.execute("ALTER TABLE system_log DROP COLUMN IF EXISTS message")
+    op.drop_column('system_log', 'message')
