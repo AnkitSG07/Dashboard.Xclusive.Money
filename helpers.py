f2e37deaae82c14e0ccd3580d3e8ab19c9358756
AnkitSG07 <ankitsinghgaria@gmail.com>
2025-07-10 10:47:49 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 from flask import session
 from sqlalchemy import or_
-from models import User, OrderMapping, Account
+from models import User, OrderMapping, Account, d
 
 
 def current_user():
@@ -32,9 +32,10 @@ def order_mappings_for_user(user):
 
 def active_children_for_master(master):
     """Return active child accounts belonging to the same user as ``master``."""
-    return Account.query.filter_by(
-        user_id=master.user_id,
-        role='child',
-        linked_master_id=master.client_id,
-        copy_status='On'
+    return Account.query.filter(
+        Account.user_id == master.user_id,
+        Account.role == 'child',
+        Account.linked_master_id == master.client_id,
+        db.func.lower(Account.copy_status) == 'on'
     ).all()
+
