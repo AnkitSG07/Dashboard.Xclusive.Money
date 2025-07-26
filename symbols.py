ba3443f7a57e922643c3dc2b1fdd2b5b5233dec0
AnkitSG07 <ankitsinghgaria@gmail.com>
2025-07-26 17:11:50 +0530

    Update symbols.py

--- symbols.py
+++ symbols.py
@@ -1,10 +1,39 @@
 from brokers.symbol_map import SYMBOL_MAP
 
+
 def get_symbols():
-    """Return list of symbol and security_id pairs from SYMBOL_MAP."""
+    """Return list of symbols with token mappings for common brokers."""
     symbols = []
     for sym, data in SYMBOL_MAP.items():
-        sec_id = data.get('dhan', {}).get('security_id')
-        if sec_id:
-            symbols.append({'symbol': sym, 'security_id': sec_id})
+        entry = {"symbol": sym}
+
+        dhan_id = data.get("dhan", {}).get("security_id")
+        if dhan_id:
+            entry["dhan"] = dhan_id
+
+        alice_id = data.get("aliceblue", {}).get("symbol_id")
+        if alice_id:
+            entry["aliceblue"] = alice_id
+
+        angel_id = (
+            data.get("angelone", {}).get("token")
+            or data.get("angel", {}).get("token")
+        )
+        if angel_id:
+            entry["angelone"] = angel_id
+
+        iifl_id = data.get("iifl", {}).get("token")
+        if iifl_id:
+            entry["iifl"] = iifl_id
+
+        kotak_id = data.get("kotakneo", {}).get("token")
+        if kotak_id:
+            entry["kotak"] = kotak_id
+
+        upstox_id = data.get("upstox", {}).get("token")
+        if upstox_id:
+            entry["upstox"] = upstox_id
+
+        if len(entry) > 1:
+            symbols.append(entry)
     return symbols
