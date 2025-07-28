ecc5c61062de1a14a7450e6f95b45985f236beac
AnkitSG07 <ankitsinghgaria@gmail.com>
2025-07-28 13:56:17 +0530

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,8 +3,24 @@ from app import parse_timestamp
 
 
 def test_parse_timestamp_iso():
-    ts = parse_timestamp('2025-07-25T06:52:12Z')
+    ts = parse_timestamp("2025-07-25T06:52:12Z")
     assert isinstance(ts, datetime.datetime)
     assert ts.year == 2025
     assert ts.month == 7
     assert ts.day == 25
+
+
+def test_parse_timestamp_numeric_str():
+    ts = parse_timestamp("1693526400")
+    assert isinstance(ts, datetime.datetime)
+    assert ts.year == 2023
+    assert ts.month == 9
+    assert ts.day == 1
+
+
+def test_parse_timestamp_ms_numeric_str():
+    ts = parse_timestamp("1693526400000")
+    assert isinstance(ts, datetime.datetime)
+    assert ts.year == 2023
+    assert ts.month == 9
+    assert ts.day == 1
